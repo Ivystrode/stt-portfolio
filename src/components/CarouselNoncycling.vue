afeb9b2704880b05759980a976b96f38412a1cc2
Ivystrode <benjamin200@hotmail.co.uk>
2022-05-08 00:58:09 +0100

    other page now show multiple projects

---
+++ src/components/CarouselNoncycling.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-carousel hide-delimiters show-arrors-on-hover>
+    <v-carousel-item
+      v-for="i in slideImages"
+      :key="i"
+      :src="i"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+  export default {
+    name: "CarouselNoncycling",
+    props: {
+        slideImages: Array
+    },
+    data: () => ({
+      model: 0,
+    }),
+    async created() {
+        console.log(this.slideImages)
+    }
+  }
+</script>
--- src/views/OtherView.vue
+++ src/views/OtherView.vue
@@ -1,42 +1,173 @@
 <template>
   <v-container>
+    <div class="descriptiontitle text-center">
+      <p>Other Projects</p>
+    </div>
+
+
+
     <v-row class="text-center">
       <v-col class="picbox mb-4 border">
-        <CarouselComp :slideImages="this.slideImages"/>
+        <CarouselNoncycling :slideImages="this.slideImages"/>
       </v-col>
       <v-col class="picbox mb-4 border">
-        <PictureComp @open-modal="openModal" :image="this.image2"/>
+        <div class="table">
+        <table class="ui celled inverted table">
+          <thead class="descriptiontitle tablehead">Project</thead>
+        <tbody>
+
+          <tr>
+            <th scope="row">Frontend</th>
+            <td>VueJS, hosted on the "Hub", accessible via VPN</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Backend</th>
+            <td>A Flask server running an API, that communicates with a mongoDB hosted on Atlas</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Computer Vision</th>
+            <td>A pre-trained model that is configured only to detect people and vehicles, and custom trained face detection that runs on a remote machine</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Hardware</th>
+            <td>Multiple RPi's communicating via sockets and 433Mhz radios, that autorun on startup</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Natural Language Processing</th>
+            <td>A neural network trained on a list of tagged "intents", predicting the intent based on the input pattern and serving a suitable response</td>
+          </tr>
+
+        </tbody>
+      </table>
+      </div>
       </v-col>
     </v-row>
+
+
+
+
     <v-row class="text-center">
       <v-col class="picbox mb-4 border">
-        <PictureComp @open-modal="openModal" :image="this.image3"/>
+        <div class="table">
+        <table class="ui celled inverted table">
+          <thead class="descriptiontitle tablehead">Project</thead>
+        <tbody>
+
+          <tr>
+            <th scope="row">Frontend</th>
+            <td>VueJS, hosted on the "Hub", accessible via VPN</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Backend</th>
+            <td>A Flask server running an API, that communicates with a mongoDB hosted on Atlas</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Computer Vision</th>
+            <td>A pre-trained model that is configured only to detect people and vehicles, and custom trained face detection that runs on a remote machine</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Hardware</th>
+            <td>Multiple RPi's communicating via sockets and 433Mhz radios, that autorun on startup</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Natural Language Processing</th>
+            <td>A neural network trained on a list of tagged "intents", predicting the intent based on the input pattern and serving a suitable response</td>
+          </tr>
+
+        </tbody>
+      </table>
+      </div>
       </v-col>
-      <v-col class="picbox mb-4 border">
+      <v-col @click="openCodeModal" class="picbox mb-4 border">
         <PictureComp @open-modal="openModal" :image="this.image4"/>
       </v-col>
+      <v-col v-if="toggleCodeModal">
+        <CodeCarousel @close-code-modal="closeCodeModal" :slideImages="this.codeImages" />
+      </v-col>
     </v-row>
+
+
+
+        <v-row class="text-center">
+      <v-col class="picbox mb-4 border">
+        <CarouselNoncycling :slideImages="this.slideImages"/>
+      </v-col>
+      <v-col class="picbox mb-4 border">
+        <div class="table">
+        <table class="ui celled inverted table">
+          <thead class="descriptiontitle tablehead">Project</thead>
+        <tbody>
+
+          <tr>
+            <th scope="row">Frontend</th>
+            <td>VueJS, hosted on the "Hub", accessible via VPN</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Backend</th>
+            <td>A Flask server running an API, that communicates with a mongoDB hosted on Atlas</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Computer Vision</th>
+            <td>A pre-trained model that is configured only to detect people and vehicles, and custom trained face detection that runs on a remote machine</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Hardware</th>
+            <td>Multiple RPi's communicating via sockets and 433Mhz radios, that autorun on startup</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Natural Language Processing</th>
+            <td>A neural network trained on a list of tagged "intents", predicting the intent based on the input pattern and serving a suitable response</td>
+          </tr>
+
+        </tbody>
+      </table>
+      </div>
+      </v-col>
+    </v-row>
+
+
+
+
+
+
       <div v-if="toggleModal">
       <PictureModal @close-modal="closeModal" :image="modalImage"/>
     </div>
+
   </v-container>
+
 </template>
 
 <script>
 import PictureComp from '../components/PictureComp.vue'
 import PictureModal from '../components/PictureModal.vue'
-import CarouselComp from '../components/CarouselComp.vue'
+import CarouselNoncycling from '../components/CarouselNoncycling.vue'
+import CodeCarousel from '../components/CodeCarousel.vue'
 
 export default {
 
-  name: 'OtherView',
+  name: 'ScraperNlp',
   components: {
     PictureComp,
     PictureModal,
-    CarouselComp
+    CarouselNoncycling,
+    CodeCarousel
   },
   data: () => ({
     toggleModal: false,
+    toggleCodeModal: false,
     modalImage: String,
 
     frontendicon: document.getElementById('frontend-icon'),
@@ -44,17 +175,29 @@ export default {
     fullstackicon: document.getElementById('fullstack-icon'),
     nlpicon: document.getElementById('nlp-icon'),
     computervisionicon: document.getElementById('cv-icon'),
+    rpiicon: document.getElementById('rpi-icon'),
 
     image1:"",
-    image2:"",
-    image3:"",
-    image4:"",
+    image2:require("@/assets/homedashboard/atlasdb.png"),
+    image3:require("@/assets/homedashboard/botdetectmsg.png"),
+    image4:require("@/assets/homedashboard/ccrfcon.png"),
     slideImages: [
-    'https://pixabay.com/get/gbbd34233a7417966a59bc7418ea5f5ee18d23e846597449bcfdcc341e2b4b026750cf227e759b5c40197c8fd8d0c5ea8ec6e64bb270359d62f1a4069967cbdc2_640.jpg',
-    'https://pixabay.com/get/gcb54ec075e17c570fe5b897e8a8ab0dcccb898826c12d1bce4a373b94a70634e97676a5e4fe9ad278e39e23dda6f042d2b0a4171ca77b4e1fffc045d092856cd_640.jpg',
-    'https://pixabay.com/get/g21514d18e61dbcb178948abc10d6eadbeacdf77632134c28784233808d3fd352cf3fcd2ddd0fff78941b8a448561fa9e_640.jpg',
-    'https://pixabay.com/get/g5b5bf95d8ab2556b695038a1b7f4cca889edd645478a1f59d6fa521e47c846e990abf4046ead5b4d71d73c01ec046b18e0c81f14314549a085aeaef487f59c09_640.jpg',
-    'https://pixabay.com/get/g5b5bf95d8ab2556b695038a1b7f4cca889edd645478a1f59d6fa521e47c846e990abf4046ead5b4d71d73c01ec046b18e0c81f14314549a085aeaef487f59c09_640.jpg',
+    "https://i.imgur.com/OkGmQkH.jpg",
+    "https://i.imgur.com/1DukZfQ.jpg",
+    "https://i.imgur.com/fnSIVL8.jpg",
+    "https://i.imgur.com/u8gDkoK.jpg",
+    "https://i.imgur.com/2lNrgp4.jpg",
+    "https://i.imgur.com/hRe3hWl.jpg",
+    "https://i.imgur.com/uWArwtH.png",
+    "https://i.imgur.com/n1ac1Of.png",
+  ],
+  codeImages: [
+    "https://i.imgur.com/Ed1HPta.png",
+    "https://i.imgur.com/iehtRVU.png",
+    "https://i.imgur.com/gv7Qy3R.png",
+    "https://i.imgur.com/Fpo0yJE.png",
+    "https://i.imgur.com/1ZHlYnr.png",
+    "https://i.imgur.com/faznl0n.png",
   ]
   }),
   methods: {
@@ -66,6 +209,14 @@ export default {
     },
     closeModal() {
       this.toggleModal = false
+    },
+    openCodeModal(img) {
+      console.log("open modal")
+      this.toggleCodeModal = true
+      console.log(img)
+    },
+    closeCodeModal() {
+      this.toggleCodeModal = false
     },    
   },
 
@@ -76,6 +227,7 @@ export default {
     this.fullstackicon.style.color = "green"
     this.nlpicon.style.color = "green"
     this.computervisionicon.style.color = "green"
+    this.rpiicon.style.color = "green"
   }
 }
 </script>
@@ -85,5 +237,16 @@ export default {
     justify-content: center;
     align-items: center;
     display: flex;
+  }
+  .descriptiontitle {
+    font-size: xx-large;
+    margin-bottom: 20px;
+  }
+  th {
+    padding-left: 8px;
+    padding-right: -8px;
+  }
+  .descriptiontitle.tablehead {
+    color: coral;
   }
 </style>
